734f995485caa901a61b5c913a8ca8f63a56805a
kaliryder <56712498+kaliryder@users.noreply.github.com>
2024-04-29 12:48:22 -0400

    update before i change

--- pointstepperupdated.py
+++ pointstepperupdated.py
@@ -2,14 +2,14 @@ import time
 import board
 import digitalio
 
-# X setup
+# Y setup
 motor1Dir = digitalio.DigitalInOut(board.A7)
 motor1Dir.direction = digitalio.Direction.OUTPUT
 
 motor1Step= digitalio.DigitalInOut(board.A6)
 motor1Step.direction = digitalio.Direction.OUTPUT
 
-# Y setup
+# X setup
 motor2Dir = digitalio.DigitalInOut(board.A3)
 motor2Dir.direction = digitalio.Direction.OUTPUT
 
@@ -27,16 +27,16 @@ currentPosY = 0
 def step(mStep,mDir,direction,motorNum):
     global currentPosX
     global currentPosY
-    if motorNum == 0:
-        if direction == True:
-            currentPosX = currentPosX + 1
-        else:
-            currentPosX = currentPosX - 1
-    elif motorNum == 1:
+    if motorNum == 1:
         if direction == True:
             currentPosY = currentPosY + 1
         else:
             currentPosY = currentPosY - 1
+    elif motorNum == 2:
+        if direction == True:
+            currentPosX = currentPosX + 1
+        else:
+            currentPosX = currentPosX - 1
     mDir.value = direction
     mStep.value = True
     time.sleep(0.001)
@@ -45,8 +45,8 @@ def step(mStep,mDir,direction,motorNum):
     print(str(currentPosX )+ " : " + str(currentPosY))
 
 # Step info setup
-xPos = [100, 500, 100]
-yPos = [100, 500, 100]
+xPos = [500, 100, 500]
+yPos = [500, 100, 500]
 posLength = 3
 posIndex = 0
 xReached = False
@@ -66,11 +66,11 @@ while True:
 
         if not xReached:
             direction = (currentPosX < xPos[posIndex])
-            step(motor1Step, motor1Dir, direction, 0)
+            step(motor2Step, motor2Dir, direction, 2)
             print(f"Stepping X to {'increase' if direction else 'decrease'}: {currentPosX}")
         if not yReached:
             direction = (currentPosY < yPos[posIndex])
-            step(motor2Step, motor2Dir, direction, 1)
+            step(motor1Step, motor1Dir, direction, 1)
             print(f"Stepping Y to {'increase' if direction else 'decrease'}: {currentPosY}")
 
         # Check if both have reached after stepping
